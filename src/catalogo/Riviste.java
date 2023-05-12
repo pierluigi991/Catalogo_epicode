9d0404c54331fb2ccad68f178494e9751519c9f1
pierluigi991 <123975572+pierluigi991@users.noreply.github.com>
2023-05-12 13:46:28 +0200

    product

--- src/catalogo/Riviste.java
+++ src/catalogo/Riviste.java
@@ -1,5 +1,33 @@
 package catalogo;
 
-public class Riviste {
+import catalogo.Periodicita;
+import catalogo.Prodotto;
+
+public class Riviste extends Prodotto {
+	private Periodicita periodicita;
+
+	public Riviste(long codiceISBN, String titolo, String annoPubblicazione, int numeroPagine,
+			Periodicita periodicita) {
+		super(codiceISBN, titolo, annoPubblicazione, numeroPagine);
+		this.periodicita = periodicita;
+	}
+
+	@Override
+	public String toString() {
+		return "[codiceISBN=" + codiceISBN + ", titolo=" + titolo + ", annoPubblicazione=" + annoPubblicazione
+				+ ", numeroPagine=" + numeroPagine + ", periodicita=" + periodicita + "]";
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public long getCodiceISBN() {
+		return codiceISBN;
+	}
+
+	public String getAnnoPubblicazione() {
+		return annoPubblicazione;
+	}
 
 }
